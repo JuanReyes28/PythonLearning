2735cad87fd175ef5c49b66ccf6e0c7b136b480f
JuanReyes28 <juanreyes28003@gmail.com>
2021-09-06 20:09:42 -0500

    Primer commit

---
+++ Bot.py
@@ -0,0 +1,6 @@
+print ("Welcome to the colombia test. \n This is a basic test about how much you know the country. ")
+
+print ("First question \n What is the typical food in Bogota? ")
+
+print ("1: Bandeja Paisa \n 2: Ajiaco \n 3: Sopa De Mondogo \n 4: Bocadillo")
+
